c120fa5457de4c6f614b8c43455b409a867a36fe
Evan Cobb <cobb.evan@gmail.com>
2016-11-12 20:13:46 -0600

    oh bebe, replacing garbage with some ES6 hotness

--- src/states/menu.js
+++ src/states/menu.js
@@ -1,15 +1,12 @@
 class Menu extends Phaser.State {
     create() {
         this.game.stage.backgroundColor = "#aaaaaa";
-        var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Start it!");
+        let text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Start it!");
         text.inputEnabled = true;
-        text.events.onInputDown.add(
-            (function(context) {
-                return function() {
-                    console.log("Moving from menu");
-                    context.game.state.start('game');
-                };
-            })(this));
+        text.events.onInputDown.add(() => {
+            console.log("Moving from menu");
+            this.game.state.start('game');
+        });
     }
 }
 
