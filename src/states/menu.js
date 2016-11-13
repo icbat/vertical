02ec6775601c2c9243ab98b2d7bf636f7419f8a9
Evan Cobb <cobb.evan@gmail.com>
2016-11-12 20:21:03 -0600

    fixing dat spacing

--- src/states/menu.js
+++ src/states/menu.js
@@ -3,6 +3,7 @@ class Menu extends Phaser.State {
         this.game.stage.backgroundColor = "#aaaaaa";
         let text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Start it!");
         text.inputEnabled = true;
+        text.anchor.setTo(0.5, 0.5);
         text.events.onInputDown.add(() => {
             console.log("Moving from menu");
             this.game.state.start('game');
