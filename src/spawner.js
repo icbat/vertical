352ab828349b3acc6468f065241d81f78cf5005c
Evan Cobb <cobb.evan@gmail.com>
2016-11-20 12:10:47 -0600

    multiple objects can spawn

--- src/spawner.js
+++ src/spawner.js
@@ -5,15 +5,18 @@ class Spawner {
     // this will be states.game, because that's the context passed to the callback
     spawn(level) {
         this.game.camera.shake(0.005, 100);
-        let obstacle = new Obstacle(this.game, this.game.rnd.pick(this.columnXVals), level);
+        let numberToSpawn = this.game.rnd.integerInRange(1, this.columnXVals.length - 2);
+        for (let i = 0; i < numberToSpawn; ++i) {
+            let obstacle = new Obstacle(this.game, this.game.rnd.pick(this.columnXVals), level);
 
-        this.obstacles.push(obstacle);
-        obstacle.destroyed.addOnce(() => {
-            this.obstacles.shift();
-            this.game.global.score += 1;
-        });
+            this.obstacles.push(obstacle);
+            obstacle.destroyed.addOnce(() => {
+                this.obstacles.shift();
+                this.game.global.score += 1;
+            });
 
-        this.game.add.existing(obstacle);
+            this.game.add.existing(obstacle);
+        }
     }
 
 }
