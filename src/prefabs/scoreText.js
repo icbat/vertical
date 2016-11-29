80bfae918c2ae84054c70123bbb092e087ce300e
Evan Cobb <cobb.evan@gmail.com>
2016-11-29 14:52:22 -0600

    beautifying file before I touch it

--- src/prefabs/scoreText.js
+++ src/prefabs/scoreText.js
@@ -2,14 +2,16 @@ import colorscheme from '../colorscheme';
 
 class ScoreText extends Phaser.Text {
 
-  constructor(game, x, y) {
-      const style = {"fill": colorscheme.fontColor};
-    super(game, x, y, "text", style);
-  }
+    constructor(game, x, y) {
+        const style = {
+            "fill": colorscheme.fontColor
+        };
+        super(game, x, y, "text", style);
+    }
 
-  update() {
-      this.text = this.game.global.score;
-  }
+    update() {
+        this.text = this.game.global.score;
+    }
 
 }
 
