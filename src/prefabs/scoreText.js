95f1ba4704f66cf862c8a4170ce6856108d1e5f0
Evan Cobb <cobb.evan@gmail.com>
2016-11-29 17:34:57 -0600

    points now update individually when you get a whole bunch

--- src/prefabs/scoreText.js
+++ src/prefabs/scoreText.js
@@ -5,10 +5,17 @@ class ScoreText extends Phaser.Text {
 
     constructor(game, x, y) {
         super(game, game.world.centerX, game.world.height * 0.1, "", new FontStyle(game));
-
         this.anchor.setTo(0.5, 0.5);
 
         this.score = 0;
+        this.updateQueue = [];
+        this.updateTimer = this.game.time.create();
+        let event = this.updateTimer.loop(Phaser.Timer.SECOND / 16, () => {
+            this.updateScore(this.updateQueue.shift());
+            if (!this.updateQueue.length) {
+                this.updateTimer.stop(false);
+            }
+        }, this);
     }
 
     update() {
@@ -17,9 +24,17 @@ class ScoreText extends Phaser.Text {
             this.scale.x = Math.max(1, this.scale.x - scalingspeed * this.game.time.elapsed / 15);
             this.scale.y = Math.max(1, this.scale.y - scalingspeed * this.game.time.elapsed / 15);
         }
+
+        if (!this.updateTimer.running && this.updateQueue.length) {
+            this.updateTimer.start();
+        }
     }
 
     scoreUp(points) {
+        this.updateQueue.push(points);
+    }
+
+    updateScore(points) {
         this.score += points;
         this.text = this.score;
         this.scale.x += 0.2;
