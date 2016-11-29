db23bfd0e6f928a487c4e8f4d3544c23210ca961
Evan Cobb <cobb.evan@gmail.com>
2016-11-29 14:49:22 -0600

    remove trails, squish player movement, and move more responsively

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,10 +1,10 @@
-import Ghost from './ghost';
 import colorscheme from '../colorscheme';
 
 class Player extends Phaser.Sprite {
 
     constructor(game, x, y, columnXVals, playerSize, endGameCallback, endGameContext) {
         super(game, x, y, 'pixel');
+        this.initialY = y;
         this.anchor.setTo(0.5, 0.5);
         this.tint = colorscheme.player;
         game.physics.enable(this, Phaser.Physics.ARCADE);
@@ -13,20 +13,32 @@ class Player extends Phaser.Sprite {
         this.columnXVals = columnXVals;
         this.col = 2;
 
-        for (let i = 0; i < 5; ++i) {
-            this.game.add.existing(new Ghost(this, i));
-        }
         this.body.onOverlap = new Phaser.Signal();
         this.body.onOverlap.addOnce(endGameCallback, endGameContext);
+        this.speed = 0;
     }
 
     update() {
         let targetX = this.columnXVals[this.col];
-        if (this.targetX != this.x) {
-            this.x += (targetX - this.x) * 0.1 * this.game.time.elapsed / 15;
+        const acceleration = 5;
+        if (targetX !== this.x) {
+            this.speed += acceleration;
+            let velocity = targetX - this.x;
+            let direction = velocity ? velocity < 0 ? -1 : 1 : 0;
+            let distance = Math.min(this.speed * this.game.time.elapsed / 15, Math.abs(velocity));
+            this.x += direction * distance;
+
+        } else {
+            this.speed = Math.max(0, this.speed - acceleration);
         }
         this.oldPositions.push(this.x);
         this.oldPositions.shift();
+
+        let squishPercent = this.speed;
+        const originalSize = 64;
+        this.height = originalSize * (1 - (squishPercent / 200));
+        this.y = this.initialY + originalSize * (1 + (squishPercent / 200)) - originalSize;
+        this.width = originalSize * (1 + (squishPercent / 100));
     }
 
     move(click) {
