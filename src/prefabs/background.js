3f05b6d5f6801295e287d0ed53d9cf7d7265d754
Evan Cobb <cobb.evan@gmail.com>
2016-12-07 12:16:36 -0600

    beautifying

--- src/prefabs/background.js
+++ src/prefabs/background.js
@@ -1,24 +1,24 @@
 class Background extends Phaser.TileSprite {
 
-  constructor(game, direction) {
-    super(game, game.world.centerX, 0, game.world.centerX * direction, game.world.height, 'tile-bg');
-    this.originalAlpha = 0.2;
-    this.alpha = this.originalAlpha;
-  }
+    constructor(game, direction) {
+        super(game, game.world.centerX, 0, game.world.centerX * direction, game.world.height, 'tile-bg');
+        this.originalAlpha = 0.2;
+        this.alpha = this.originalAlpha;
+    }
 
-  update() {
+    update() {
 
-  }
+    }
 
-  touched() {
-      this.alpha = 0.8;
+    touched() {
+        this.alpha = 0.8;
 
-      let bgFadeTween = this.game.add.tween(this);
-      bgFadeTween.to({
-          alpha: this.originalAlpha
-      }, 150);
-      bgFadeTween.start();
-  }
+        let bgFadeTween = this.game.add.tween(this);
+        bgFadeTween.to({
+            alpha: this.originalAlpha
+        }, 150);
+        bgFadeTween.start();
+    }
 
 }
 
