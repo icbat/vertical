ad2fe93152b706315c0153898d5f81a44a7d40e4
Evan Cobb <cobb.evan@gmail.com>
2016-12-07 15:04:24 -0600

    tweening button in on start

--- src/states/menu.js
+++ src/states/menu.js
@@ -4,11 +4,21 @@ import HighScoreText from '../prefabs/highScoreText';
 
 class Menu extends Phaser.State {
     create() {
-        new TextButton(this.game, 'Start it!', this.game.world.centerX, this.game.world.centerY);
+        const startButton = new TextButton(this.game, 'Start it!', this.game.world.centerX, -100);
 
         const highScore = localStorage.getItem('vertical-highScore');
         const scoreText = new HighScoreText(this.game, highScore);
         this.game.analytics.getDailyHighScore(scoreText.updateDailyHighScore, scoreText);
+
+        let buttonTween = this.game.add.tween(startButton).to({
+            y: this.game.world.centerY
+        }, Phaser.Timer.SECOND / 4, Phaser.Easing.Elastic.Out);
+
+        let lagTimer = this.game.time.create();
+        lagTimer.add(Phaser.Timer.SECOND / 4, () => {
+            buttonTween.start();
+        });
+        lagTimer.start();
     }
 }
 
