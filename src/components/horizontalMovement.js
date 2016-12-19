f2808d45467012f88fedb60554677eb33d0dcc80
Evan Cobb <cobb.evan@gmail.com>
2016-12-19 15:27:13 -0600

    standardizing the physics delta between horizontal/vertical components

--- src/components/horizontalMovement.js
+++ src/components/horizontalMovement.js
@@ -5,7 +5,7 @@ class HorizontalMovement extends Component {
 
     update(entity, game) {
         if (!!this.targetX && this.targetX != entity.x) {
-            entity.x += (this.targetX - entity.x) * 0.1 * game.time.physicsElapsed * game.time.desiredFps;
+            entity.x += (this.targetX - entity.x) * 0.1 * game.time.elapsed / config.physics.delta;
         }
     }
     activate(level) {
